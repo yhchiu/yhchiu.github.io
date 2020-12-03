92781d8a7acd2b3d33cfdf05d1c2d800211f7626
Arthur Chiu <github_ns@artchiu.org>
2020-12-04 06:07:59 +0800

    Add files via upload

---
+++ edu/20201204/Lab101.py
@@ -0,0 +1,32 @@
+from machine import Pin
+import time
+import network, urequests
+import dht
+
+ifttt_key = "IFTTT的金鑰"
+
+sensor = dht.DHT11(Pin(0))                 # 使用 D3 腳位取得溫溼度物件
+
+sta_if = network.WLAN(network.STA_IF)     # 取得無線網路介面
+sta_if.active(True)                       # 啟用無線網路
+sta_if.connect('無線網路名稱', '密碼')     # 連結無線網路
+while not sta_if.isconnected():           # 等待無線網路連上
+    pass
+print("connected")
+
+msg_sent = False
+while True:
+    sensor.measure()
+    temperature = sensor.temperature()
+
+    if temperature > 20:
+        if not msg_sent:
+            print("發送緊報!")
+            urequests.get("https://maker.ifttt.com/trigger/fire/"
+                          "with/key/%s?value1=%2d" % (
+                          ifttt_key, temperature))
+            msg_sent = True
+    else:
+        msg_sent = False
+
+    time.sleep(3)
